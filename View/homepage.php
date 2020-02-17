ae8ee4dd1ac1b2a63bef31e4bf2727e3659275ce
emma <emma.wishart@live.com.au>
2020-02-17 11:11:17 +0100

    adding basic buttons

--- View/homepage.php
+++ View/homepage.php
@@ -6,13 +6,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href = "style.css" type="text/css" rel="stylesheet"/>
     <title>Becode - Boiler plate MVC</title>
 </head>
 <body>
 <?php require 'includes/header.php'?>
 <section>
     <h4>Hello <?php echo $user->getName()?>,</h4>
-    <p>Put your content here.</p>
+    <form method="get">
+        <button name="customer" class="drpbutton" type="submit">Customer</button>
+        <button name="product" class="drpbutton" type="submit">Products</button>
+    </form>
 </section>
 <?php require 'includes/footer.php'?>
 </body>
