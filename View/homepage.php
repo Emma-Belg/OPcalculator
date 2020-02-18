9b298cc0ddfdc91114eff2d8a7b0f71215412cc5
emma <emma.wishart@live.com.au>
2020-02-18 10:17:04 +0100

    working on getting selected item

--- View/homepage.php
+++ View/homepage.php
@@ -17,43 +17,28 @@ require 'Model/SelectButton.php' ?>
 
 <section>
     <h1>Helloooooooooo</h1>
-    <form method="get">
+    <form action="#"  method="get">
         <label>
-            <select name="customer" class="drpbutton">
-
-        <?php
-        $customerData = json_decode(file_get_contents("customers.json"),true);
-
-        $allCustomers = [];
-        foreach ($customerData as $row) {
-            $allCustomers[] = new SelectButton($row['name']);
-            echo "<option value='customer-name'>".$row["name"]  ."</option>";
-        }
-        if(isset($_GET["customer"])){
-            $selected_val = $_GET["customer"];
-            echo "Selected Customer is:". $selected_val;
-        }
-
-        ?>
-
+            <select name="customer">
                 <option></option>
                 <?php
                 $selectButton1 = new SelectButton();
                 $selectButton1->getInfo(json_decode(file_get_contents("customers.json"), true));
+
+                if(isset($_GET["submit"])){
+                    $selected_val = $_GET["customer"];
+                    echo "Selected Customer is:". $selected_val;
+                }
                 ?>
 
             </select>
         </label>
         <label>
             <select name="product" class="drpbutton">
+                <option></option>
                 <?php
-                $productsData = json_decode(file_get_contents("products.json"),true);
-
-                $allProducts = [];
-                foreach ($productsData as $row) {
-                    $allProducts[] = new Products($row['name']);
-                    echo "<option value='Products-name'>".$row["name"]  ."</option>";
-                }
+                $selectButton1 = new SelectButton();
+                $selectButton1->getInfo(json_decode(file_get_contents("products.json"), true));
                 ?>
             </select >
         </label>
