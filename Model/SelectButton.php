094092fb586791c43d9dc7c7d8845d152a1278da
Emma-Belg <57953767+Emma-Belg@users.noreply.github.com>
2020-02-19 14:48:31 +0100

    getting working version online

--- Model/SelectButton.php
+++ Model/SelectButton.php
@@ -1,28 +1,28 @@
 <?php
 declare(strict_types = 1);
 
+
 class SelectButton
 {
     private $selectedCostumer;
-
     private $selectedProduct = "";
-
-    private $infoArray = [];
     private $usedProdValues = [];
+    private $infoArray =[];
 
-    public function getInfo($jsonFile) {
-        foreach ($jsonFile as $row) {
-            $this->infoArray[] = $row['name'];
-            echo "<option>". $row["name"]."</option>";
+    public function displayDropDownInfo($session) {
+        foreach ($session as $key=>$row) {
+            $this->infoArray[$key] = $row['name'];
+            var_dump( $this->infoArray[$key]);
+            //echo "<option value='name'>".$row["name"]  ."</option>";
+            echo "<option>".$row['name']  ."</option>";
         }
     }
 
-    public function getObject($selectedProd) {
-        $prodOb = (json_decode(file_get_contents("products.json"), true));
-        foreach ($prodOb as $row) {
+    public function displaySelectedProd($selectedProd) {
+        foreach ($_SESSION['productObj'] as $row) {
             if($selectedProd == $row['name']){
                 $this->usedProdValues[] = $row['name']; $row['price']; $row['description'];
-                echo "<p>". $row["name"]. $row['price'] . $row['description'] . "</p>";
+                echo "<p><b>Product Name: </b>". $row["name"]. "<br><b>Price: </b> €".$row['price'] ."<br> <b>Description: </b>".  $row['description'] . "</p>";
             }
         }
     }
@@ -32,22 +32,12 @@ class SelectButton
         if(isset($_GET['submit'])){
             $this->selectedCostumer = $_GET['customer'];
             $this->selectedProduct = $_GET['product'];
-            echo "<br>Selected: " . $this->selectedCostumer . ' ' . $this->selectedProduct . "<br>";
-            self :: getObject($this->selectedProduct);
+            echo "<br>Selected: " . $this->selectedCostumer . ', ' . $this->selectedProduct . "<br>";
+            self :: displaySelectedProd($this->selectedProduct);
         }
 
     }
 
-   /* public function testGetInfo($jsonFile) {
-        foreach ($jsonFile as $row) {
-            if($this->selectedProduct == $row['name']){
-                $this->usedProdValues[] = $row['name']; $row['price']; $row['description'];
-                echo "<p>". $row["name"]. $row['price'] . $row['description'] . "</p>";
-            }
-
-        }
-        return $this->usedProdValues;
-    }*/
     public function showProduct(){
 
         //should this actually be on the submit or the below??
