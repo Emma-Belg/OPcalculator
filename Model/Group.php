b7213f34f1636cd06bb1f46e87b04fe7d922e25d
Tim-Smet <tim2smet@gmail.com>
2020-02-19 16:49:38 +0100

    Trying to calculate price after discount

--- Model/Group.php
+++ Model/Group.php
@@ -11,20 +11,38 @@ class Group
 
     private $fixedDiscount = [];
 
-    private $maxFixedValue;
+    private $sumFixedValue = 0;
 
-    private $maxVariableValue;
+    private $maxVariableValue = 0;
+
+    private $selectedProd;
+
+    private $price;
 
     public function getIdSelectedCust($selectedCust){
         $custOb = (json_decode(file_get_contents("customers.json"), true));
         $groupOb = (json_decode(file_get_contents("groups.json"), true));
         $productsOb = (json_decode(file_get_contents("products.json"), true));
+
+        //Get price of selected product
+        $price = 0;
+        if(isset($_GET['submit'])){
+            $this->selectedProd = $_GET['product'];
+            foreach ($productsOb as $row){
+                if($this->selectedProd == $row['price']){
+                    $price = $row['price'];
+                }
+            }
+        }
+
+        //Get group id of selected person
         foreach ($custOb as $row) {
             if($selectedCust == $row['name']){
                 $this->idSelectedCust = $row['group_id'];
             }
         }
 
+        //Get the discounts
         foreach ($groupOb as $row){
             if($this->idSelectedCust == $row['id']){
                 echo 'name of group: ' . $row['name'] . '<br>';
@@ -38,13 +56,25 @@ class Group
                 $this->idSelectedCust = $row['group_id'];
             }
         }
+
+        //Calculating price after discount
         if(!empty($this->fixedDiscount)){
-            $this->maxFixedValue = max($this->fixedDiscount);
+            $this->sumFixedValue = array_sum($this->fixedDiscount);
+            if($this->sumFixedValue < $price){
+                $this->sumFixedValue = $price - $this->sumFixedValue;
+            }
         }
         if(!empty($this->variableDiscount)){
             $this->maxVariableValue = max($this->variableDiscount);
+            $this->maxVariableValue = $price - ($price * ($this->maxVariableValue / 100));
         }
-        print_r($productsOb[0]['price']);
+
+        if($this->sumFixedValue > $this->maxVariableValue){
+            echo 'The price after discount is: ' . $this->sumFixedValue;
+        } else {
+            echo 'The price after discount is: ' . $this->maxVariableValue;
+        }
+
     }
 
     public function getSelectedCust()
